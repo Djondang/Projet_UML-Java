3c7316b71a97d26362a99a817f8f20d72a164e3d
kanaa <kanaa@DESKTOP-6UALB86>
2021-05-26 16:46:25 +0100

    esthétique

--- model/src/main/java/model/DBProperties.java
+++ model/src/main/java/model/DBProperties.java
@@ -18,13 +18,13 @@ class DBProperties extends Properties {
 	private final static String	PROPERTIES_FILE_NAME	= "model.properties";
 
 	/** The url. */
-	private String							url										= "";
+	private String							url= "http://localhost/phpmyadmin/#PMAURL-0:index.php?db=&table=&server=1&target=&token=26d7a3ea449af16f2b9837098282881d";
 
 	/** The login. */
-	private String							login									= "";
+	private String							login= "root";
 
 	/** The password. */
-	private String							password							= "";
+	private String							password= "";
 
 	/**
 	 * Instantiates a new DB properties.
